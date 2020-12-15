506b62ff014920ff67423302a0d57e4c4643afd0
unknown <user323@ALCHEMYCLOUD.CO.IN>
2020-12-15 16:46:46 +0530

    First commit

---
+++ src/test/java/com/cloud/eureka/SpringEurekaServer/SpringEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.eureka.SpringEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
